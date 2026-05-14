0c72de552dff3366a40da411f9e316e33431e08c
rishikanandi23 <24155416@kiit.ac.in>
2026-05-14 21:58:11 +0530

    feat: add delete

--- src/gui/ViewStudents.java
+++ src/gui/ViewStudents.java
@@ -4,17 +4,30 @@ import database.DBConnection;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
-public class ViewStudents extends JFrame {
+public class ViewStudents extends JFrame implements ActionListener {
 
     JTable table;
 
     JScrollPane scrollPane;
 
+    JButton deleteButton;
+
     public ViewStudents() {
+
+        deleteButton = new JButton("Delete Student");deleteButton = new JButton("Delete Student");
+
+        deleteButton.setBounds(250, 330, 180, 40);
+
+        add(deleteButton);
+
+        deleteButton.addActionListener(this);
 
         setTitle("All Students");
 
@@ -83,5 +96,51 @@ public class ViewStudents extends JFrame {
         }
 
         setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == deleteButton) {
+
+            int selectedRow = table.getSelectedRow();
+
+            if(selectedRow == -1) {
+
+                JOptionPane.showMessageDialog(this,
+                        "Please select a student");
+
+                return;
+            }
+
+            int id = (int) table.getValueAt(selectedRow, 0);
+
+            try {
+
+                Connection con = DBConnection.getConnection();
+
+                String query =
+                        "DELETE FROM students WHERE id=?";
+
+                PreparedStatement ps =
+                        con.prepareStatement(query);
+
+                ps.setInt(1, id);
+
+                ps.executeUpdate();
+
+                JOptionPane.showMessageDialog(this,
+                        "Student Deleted!");
+
+                con.close();
+
+                dispose();
+
+                new ViewStudents();
+
+            } catch(Exception ex) {
+
+                ex.printStackTrace();
+            }
+        }
     }
 }
